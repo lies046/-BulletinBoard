75bc7a8a8abc8af0133031c2bf1e47e8352d4481
lies046 <skytree.skysky@gmail.com>
2020-07-17 17:51:41 +0900

    first commit

---
+++ bbs.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+  <html lang="ja">
+
+    <head>
+      <meta charset="utf-8">
+      <title>掲示板</title>
+    </head>
+    
+    <body>
+      <h1>掲示板</h1>
+
+      <h2>投稿フォーム</h2>
+      <p>ここに投稿フォームを追加</p>
+      <h2>発言リスト</h2>
+        <table>
+          <tr></tr>
+            <th>id</th>
+            <th>日時</th>
+            <th>投稿内容</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>2020-07-17</td>
+            <td>こんにちは</td>
+          </tr>
+        </table>
+    </body>
+</html>

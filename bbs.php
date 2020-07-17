9e56ffae09e3e77e26374676a86bb27560b9a20e
lies046 <skytree.skysky@gmail.com>
2020-07-17 21:05:13 +0900

    Add Delete

--- bbs.php
+++ bbs.php
@@ -12,7 +12,16 @@
         $pdo = new PDO("mysql:host=127.0.0.1;dbname=lesson;charset=utf8", "root", "");
 
         //受け取ったデータを表示
-        //print_r($_POST);
+        print_r($_POST);
+
+        //受け取ったidのレコードを削除
+        if(isset($_POST["delete_id"])){
+          $delete_id = $_POST["delete_id"];
+          $sql = "DELETE FROM bbs WHERE id = :delete_id;";
+          $stmt = $pdo->prepare($sql);
+          $stmt->bindValue(":delete_id", $delete_id, PDO::PARAM_INT);
+          $stmt->execute();
+        }
 
         if(isset($_POST["content"])){
           $content = $_POST["content"];
@@ -45,6 +54,7 @@
             <th>id</th>
             <th>日時</th>
             <th>投稿内容</th>
+            <th></th>
           </tr>
            <?php  
           //取得したデータを表示する
@@ -53,6 +63,12 @@
             <td><?=$row["id"] ?></td>
             <td><?=$row["updated_at"] ?></td>
             <td><?=$row["content"] ?></td>
+            <td>
+              <form action="bbs.php" method="post">
+                <input type="hidden" name="delete_id" value=<?= $row["id"] ?>>
+                <button type="submit">削除</button>
+              </form>
+            </td>
           </tr>
         <?php } ?>
         </table>
